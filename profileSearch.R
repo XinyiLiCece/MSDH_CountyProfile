f97a44d648b992a078ee8b34db2fa6e5135a48c4
Li <Xinyi.Li@msdh.ms.gov>
2019-04-25 16:51:15 -0500

    move searchProfile module function out of countyProfile.R

--- profileSearch.R
+++ profileSearch.R
@@ -110,3 +110,73 @@
 #   })
 #   
 # }
+
+
+profileSearchUI <- function(id, inputText) {
+  ns <- NS(id)
+  tags$div(class = "tabPanel-profileSearch",
+           # tryUI(ns("tyh")),
+           actionButton(ns("tryRouter"), "Try"
+           ),
+           # router_ui(),
+           tags$div(class = "search-container",
+                    searchBoxUI(ns("Box")),
+                    # # htmlOutput(ns("searchMap")),
+                    actionButton(ns("searchBtn"), "Go")
+           ),
+           htmlOutput(ns("renderPage"))
+           # router_ui()
+  )
+}
+
+
+profileSearch <- function(input, output, session) {
+  # callModule(try, "tyh")
+  ns <- session$ns
+  
+  
+  
+  # router(input, output, session)
+  
+  data <- read.csv(file = "CountyNameinMS.csv", header = TRUE)
+  
+  countyNameData <- as.list(data[["County"]])
+  
+  selectedCounty <- callModule(searchBox, "Box", countyNameData)
+  
+  selected <- reactiveValues(
+    name = NULL
+  )
+  
+  observe({
+    selected$name <- selectedCounty$countyName
+  })
+  
+  # sB <- reactive({
+  #   return(input$searchBtn)
+  # })
+  
+  sB <- reactive({
+    return(input$searchBtn)
+  })
+  #
+  observeEvent(input$tryRouter, {
+    change_page("side")
+  })
+  
+  # observeEvent(sB(), {
+  #   output$renderPage <- renderUI({
+  #     profilePageUI(ns("renderprofile"))
+  #   })
+  #   callModule(profilePage, "renderprofile", selected$name)
+  # })
+  #
+  observeEvent(sB(), {
+    output$renderPage <- renderUI({
+      profilePageUI(ns("renderprofile"))
+    })
+    callModule(profilePage, "renderprofile")
+  })
+  
+}
+
